c2a4eada26de7be1083480c094b541159b0f1234
Aakanxa Modha <142956130+aakanxamodha@users.noreply.github.com>
2024-02-18 01:36:09 +0530

    Created

---
+++ HackerRank-Linux-Shell/The_World_of_Numbers.sh
@@ -0,0 +1,10 @@
+#Given two integers, X and Y, find their sum, difference, product, and quotient.
+
+#!/bin/bash
+read x
+read y
+
+echo "$((x + y))"
+echo "$((x - y))"
+echo "$((x * y))"
+echo "$((x / y))"
